a0175651780fa6b96b3e87bfc48d5c3fde4becc1
ShinSub98 <wjdtjq26@naver.com>
2024-02-15 23:28:40 +0900

    feat: 유저 모델 생성

--- MoC/settings.py
+++ MoC/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config
 import os
 import pymysql
+from datetime import timedelta
 
 pymysql.install_as_MySQLdb()
 
@@ -131,14 +132,50 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+
+AUTH_USER_MODEL = 'accounts.User'
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days = 15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': True,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'JTI_CLAIM': 'jti',
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
---
+++ accounts/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
+# Create your models here.
+
+class UserManager(BaseUserManager):
+    """
+    유저 생성 매니저
+    """
+    def create_user(self, google_id, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+
+        # 사용자 생성 로직
+        user = self.model(google_id=google_id, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, google_id, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        # 슈퍼유저 생성 로직
+        return self.create_user(google_id, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    유저 모델
+    """
+    PROFILE_IMAGE_CHOICES = (
+        ('KAKAO', '카카오 프로필 사진'),
+        ('NATIVE', 'MoC 프로필 사진')
+    )
+    kakao_id = models.CharField(max_length=30, unique=True, null = True)
+    nickname = models.CharField(max_length=30, null = True) # 한글 10글자
+    native_profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    kakao_profile_image = models.TextField(blank=True, null=True)
+    profile_image_type = models.CharField(max_length = 60, choices = PROFILE_IMAGE_CHOICES, default= 'KAKAO')
+    USERNAME_FIELD = 'kakao_id'
+    EMAIL_FIELD = None
+    REQUIRED_FIELDS = []
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.nickname
+    
+    @property
+    def profile_image(self):
+        if self.profile_image_type == 'KAKAO':
+            return self.kakao_profile_image
+        elif self.profile_image_type == 'NATIVE':
+            return settings.MEDIA_URL + str(self.native_profile_image)
+        else:
+            return None
+    
+    class Meta:
+        db_table = 'user'
