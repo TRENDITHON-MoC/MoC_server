5c1ee0a3da82b9b0c4cd24c2d82640089ddd02fa
ShinSub98 <wjdtjq26@naver.com>
2024-02-15 23:30:13 +0900

    feat: 유저 시리얼라이저 추가

---
+++ accounts/serializers/user_serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from models import User
+
+class UserCreateSerailizer(serializers.ModelSerializer):
+    """
+    유저 생성 시리얼라이저
+    """
+    class Meta:
+        model = User
+        fields = ['kakao_id', 'nickname', 'kakao_profile_image']
+
+
+class UserResponseSerializer(serializers.ModelSerializer):
+    """
+    유저 정보 응답 시리얼라이저
+    """
+    class Meta:
+        model = User
+        fields = ['id', 'nickname', 'profile_image']
