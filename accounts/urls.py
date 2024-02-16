0a956ba40c5b800686c74d3ab2e3712b8d555f01
ShinSub98 <wjdtjq26@naver.com>
2024-02-16 13:34:00 +0900

    feat: 로그아웃 뷰 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,8 @@ from accounts.views import *
 app_name = 'accounts'
 
 urlpatterns = [
-    path('dev/kakao/', DevKakaoLoginView.as_view()),
-    path('kakao/login/callback/', DevKaKaoCallbackView.as_view()),
-    path('token/refresh/', TokenRefreshView.as_view()),
+    path('dev/kakao/', DevKakaoLoginView.as_view()), # 개발자 로그인
+    path('kakao/login/callback/', DevKaKaoCallbackView.as_view()), # 개발자 로그인 콜백
+    path('token/refresh/', TokenRefreshView.as_view()), # 토큰 리프레시
+    path('token/logout/', LogoutView.as_view()), # 로그아웃
 ]
--- accounts/views/token_views.py
+++ accounts/views/token_views.py
@@ -1,7 +1,11 @@
+from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import *
+
 
 class CustomTokenRefreshView(TokenRefreshView):
     """
@@ -16,3 +20,25 @@ class CustomTokenRefreshView(TokenRefreshView):
             raise InvalidToken(e.args[0])
 
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+
+class LogoutView(APIView):
+    """
+    리프레시 토큰을 무효화하여 로그아웃 하는 뷰
+    """
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            res = {
+                "msg" : "로그아웃 성공"
+            }
+            return Response(res, status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            res = {
+                "msg" : "로그아웃 실패"
+            }
+            return Response(res, status=status.HTTP_400_BAD_REQUEST)
