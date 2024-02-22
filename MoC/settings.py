7df4da51655bca951facc5de8531bafd5fc4febb
ShinSub98 <wjdtjq26@naver.com>
2024-02-22 23:14:22 +0900

    chore: 액세스 토큰 유효기간 조정

--- MoC/settings.py
+++ MoC/settings.py
@@ -150,7 +150,7 @@ AUTH_USER_MODEL = 'accounts.User'
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
